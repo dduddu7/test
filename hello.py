ae0de116eb5f1e5e15ccfcd09be524c6055d0ec3
leeseungju <lsj276324@gmail.com>
2023-11-03 10:11:06 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,53 @@
+# print("Hello World")
+
+# print("100 + 100")
+# print("%d" % (100+100))
+
+# print("%d / %d = %d" % (100,200,0.5))
+
+# print("\"C:\\Download\\\'hello\'.py\"")
+
+# print("print(\"Hello\\nWorld\")")
+
+# c = input()
+# print(c)
+
+# n = input()
+# n = int(n)
+# print(n)
+
+# f = input()
+# f = float(f)
+# print(f)
+
+# a = input()
+# b = input()
+# a = int(a)
+# b = int(b)
+# print(b)
+# print(a)
+
+# a = input()
+# a = float(a)
+# print(a)
+# print(a)
+# print(a)
+
+# a, b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+
+# a,b=input().split()
+# print(b,a)
+
+
+# a=5; b=3
+# print(a+b, a-b, a*b, a/b, a//b, a%b, a**b)
+
+# a,b,c =2,3,4
+# print(a+b+c, a+b*c, a*b/c)
+
+a=100; b=100.123
+str(a) + '1'; str(b) + '1'
